b3746362b6c6abd1741bfa8135d6f32390b08b50
Thiago Faro <thfaro.ads@gmail.com>
2023-11-21 00:15:11 -0300

    commit

---
+++ Estrutura de Dados/tuplas.py
@@ -0,0 +1,7 @@
+# Tupla é igual a lista, porem imutavel
+
+# Usando parenteses. OBS: colocar uma virgula no final
+frutas = ('maçã','banana','morango',)
+
+# Usando o comando tuple
+letras = tuple("Python")
