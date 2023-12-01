1b69bd8ba71fff92d8dd4c5e966efc03355c0ecf
Thiago Faro <thfaro.ads@gmail.com>
2023-12-01 13:10:56 -0300

    commit

--- Estrutura de Dados/tuplas.py
+++ Estrutura de Dados/tuplas.py
@@ -2,6 +2,15 @@
 
 # Usando parenteses. OBS: colocar uma virgula no final
 frutas = ('maçã','banana','morango',)
+print (frutas)
+
+pais = ("Brasil",)
+print (pais)
 
 # Usando o comando tuple
 letras = tuple("Python")
+print(letras)
+
+numeros = tuple([1,2,3,4,5,6])
+print(numeros)
+
--- Manipulação_Strings/fatiamento.py
+++ Manipulação_Strings/fatiamento.py
@@ -6,3 +6,4 @@ print (nome[7:11]) # imprime do setimo o decimo primeiro caractere
 print (nome[7:19:2]) # imprime os caracteres desejados e estipula de quantos em quantos caracteres serao exibidos
 print (nome[:]) # imprime tudo
 print (nome[::-1]) # imprime ao contrario
+
